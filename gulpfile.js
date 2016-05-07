6895d5c2aa66c8aa2d8cd2e9a86024357cc49222
Ivan <sschengyf@gmail.com>
2016-05-07 18:37:44 +1200

    Adopt babel

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,16 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  js: ['./www/js/*.js']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -26,8 +30,18 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+gulp.task('js', function() {
+  return gulp.src("www/js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("timezone.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("www/js/dist"));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.js, ['js']);
 });
 
 gulp.task('install', ['git-check'], function() {
